3019690be9321e9258f9e662a0cc09477bbe8c24
Ross Foley <ross3141@gmail.com>
2011-06-19 21:15:42 -0400

    Updated styling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,4 +47,5 @@ GpaCalculator::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = { :host => '' }
+  Sass::Plugin.options[:style] = :compressed
 end
